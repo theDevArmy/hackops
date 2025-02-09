cfa4cf7cc0c82e3db5162370a6c1defbe026e8d7
Aman Janwani <amanjanwani1486@gmail.com>
2025-02-09 13:02:43 +0530

    feat: updated metadeta

--- hackops-web/src/app/layout.js
+++ hackops-web/src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
   title: "HackOps - The Coder's Battlefield By theDevArmy",
   description: "Hackops is a community-run hackathon organized by theDevArmy. It is a 36 hours of coffee-fueled competition, focused on solving real-world challenges with incubators and accelerators.",
   author: "theDevArmy",
-  keywords: "hackathon login, coding, hackathon near me, Ahmedabad, AIML, Tech",
+  keywords: "Free hackathon, Devfolio, coding, Ahmedabad, AIML, Tech, Smart India Hackathon",
   // Open Graph metadata
   openGraph: {
     type: "website", // or "article" depending on content
@@ -24,7 +24,7 @@ export const metadata = {
     url: "https://www.hackops.dev/", // Replace with the actual URL
     images: [
       {
-        url: 'https://res.cloudinary.com/dscq2qcch/image/upload/v1738856244/hackops/Untitled_design_19_ob0euk.png',
+        url: 'https://res.cloudinary.com/dscq2qcch/image/upload/v1739086327/Pitch_ka_final_wala_deck_nuqsg1.png',
         width: 1200,
         height: 630,
         alt: "HackOps Logo - Coder's Battlefield",
@@ -38,7 +38,7 @@ export const metadata = {
     card: "summary_large_image", // For large image preview
     title: "HackOps - The Coder's Battlefield",
     description: "Hackops is a community-run hackathon organized by theDevArmy. It is a 36 hours of coffee-fueled competition, focused on solving real-world challenges with incubators and accelerators.",
-    image: "https://res.cloudinary.com/dscq2qcch/image/upload/v1738856244/hackops/Untitled_design_19_ob0euk.png", // Replace with the image URL
+    image: "https://res.cloudinary.com/dscq2qcch/image/upload/v1739086327/Pitch_ka_final_wala_deck_nuqsg1.png", // Replace with the image URL
     creator: "@theDevArmy", // Replace with Twitter handle
   },
 
